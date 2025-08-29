ea2485ad1f85508c4a1fcff821f157fe0da71394
Thomas Brooks <tgbrooks@gmail.com>
2025-08-29 09:54:37 -0400

    Add timeout to 'new' command

--- marimo/_cli/cli.py
+++ marimo/_cli/cli.py
@@ -704,6 +704,14 @@ new_help_msg = "\n".join(
     help="Enable skew protection middleware to prevent version mismatch issues.",
 )
 @click.argument("prompt", required=False)
+@click.option(
+    "--timeout",
+    required=False,
+    default=None,
+    show_default=False,
+    type=float,
+    help="Enable a global timeout to shut down the server after specified number of minutes of no connection",
+)
 def new(
     port: Optional[int],
     host: str,
@@ -715,6 +723,7 @@ def new(
     sandbox: Optional[bool],
     skew_protection: bool,
     prompt: Optional[str],
+    timeout: Optional[float],
 ) -> None:
     if sandbox:
         from marimo._cli.sandbox import run_in_sandbox
@@ -792,6 +801,7 @@ def new(
         base_url=base_url,
         redirect_console_to_browser=True,
         ttl_seconds=None,
+        timeout=timeout,
     )
 
 
